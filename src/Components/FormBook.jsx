bf1f0d5320f79999958e9464008e78d49d252191
Erielson  da silva de souza <105677162+erielsonsilva@users.noreply.github.com>
2022-08-09 21:14:15 -0300

    inicio de tabela

--- src/Components/FormBook.jsx
+++ src/Components/FormBook.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 
 export function FormBook() {
   const [validated, setValidated] = useState(false);
+  let [estados, setEstados] = useState([])
+  let [cidades, setCidades] = useState([])
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -18,8 +19,38 @@ export function FormBook() {
     setValidated(true);
   };
 
+  const fetchEstados = () => {
+    //por padrão o mètodo fetch é o GET
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+
+      .then(Response => Response.json())
+      .then(data => {
+        // console.log('estados', data)
+        setEstados(data);
+      })
+  }
+
+  const fetchCidades = sigla => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${sigla}/municipios`)
+      .then(Response => Response.json())
+      .then(data => {
+        setCidades(data.map(city => <option>{city.nome}</option>))
+      })
+  }
+
+
+  useEffect(() => {
+    fetchEstados();
+    console.log('useEffect');
+  }, [])
+
+
+
+
+
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      {console.log('return')}
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>Titulo</Form.Label>
@@ -27,7 +58,7 @@ export function FormBook() {
             required
             type="text"
             placeholder="titulo"
-           // defaultValue="Mark"
+          // defaultValue="Mark"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
@@ -41,48 +72,33 @@ export function FormBook() {
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        {/* <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group> */}
+
       </Row>
       <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
+
         <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
+          <Form.Label>Estado</Form.Label>
+          <Form.Select aria-label="Default select example" onChange={e => {
+            let sigla = e.target.value;
+            fetchCidades(sigla)
+
+          }}>
+            <option>Selecione..</option>
+            {estados.map(estado => <option value={estado.sigla}>{estado.nome}</option>)}
+
+          </Form.Select>
         </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
+
+        <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Label>cidade</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option>Selecione..</option>
+            {cidades}
+          </Form.Select>
         </Form.Group>
+
       </Row>
-     
       <Button type="submit">Submit form</Button>
     </Form>
   );
 }
-
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -36,4 +36,3 @@ export  function NavBar() {
     </Navbar>
   );
 }
-
